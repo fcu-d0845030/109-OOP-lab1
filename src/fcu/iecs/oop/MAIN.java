b7a1278003a2689f0966c49d2493bc8f6e4f9422
user <user@EA12>
2020-09-29 09:38:14 +0800

    第一版程式碼

---
+++ src/fcu/iecs/oop/MAIN.java
@@ -0,0 +1,12 @@
+package fcu.iecs.oop;
+
+public class MAIN {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I'm ªL©û¥¿");
+		System.out.println("My nid is D0845030");
+		System.out.println("This is lab 1 hw");
+	}
+
+}
