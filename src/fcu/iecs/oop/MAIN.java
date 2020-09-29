a2f73866f4f53c0715ff838e8620fd733b4833be
user <user@EA12>
2020-09-29 09:39:42 +0800

    hw

--- src/fcu/iecs/oop/MAIN.java
+++ src/fcu/iecs/oop/MAIN.java
@@ -4,7 +4,7 @@ public class MAIN {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("I'm ªL©û¥¿");
+		System.out.println("I'm Oscar");
 		System.out.println("My nid is D0845030");
 		System.out.println("This is lab 1 hw");
 	}
